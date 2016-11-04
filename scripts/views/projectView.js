e1c83560bb6d67578a67cbca66281de010af7020
Peter Kim <peterkim23@gmail.com>
2016-11-04 13:05:09 -0700

    refactored projectView.js and project.js.added AJAX and IIFE both files. Now View More truncate working. About and Contact sections now hide on page load.

--- scripts/views/projectView.js
+++ scripts/views/projectView.js
@@ -1,48 +1,52 @@
-var projectView = {};
+(function(module) {
+  var projectView = {};
 
-projectView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function(e) {
-    $('.tab-content').hide();
-    var tab = $(this).attr('data-content');
-    console.log(tab);
-    $('#' + tab).fadeIn();
-  });
-  $('.main-nav .tab:first').click();
-};
-
-projectView.setPreview = function() {
-  $('.article-body *:nth-of-type(n+2)').hide();
-  $('.view-more').on('click', function(event){
-    event.preventDefault();
-    if($(this).text() === 'View More') {
-      $(this).parent().find('*').fadeIn();
-      $(this).html('Show Less');
-    } else {
-      $('body').animate({
-        scrollTop: ($(this).parent().offset().top)
-      },200);
-      $(this).html('View More');
-      $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
-    }
-  });
-};
-
-projectView.render = function() {
-  project.forEach(function(a) {
-    $('pre code').each(function(i, block) {
-      hljs.highlightBlock(block);
+  projectView.handleMainNav = function() {
+    $('.main-nav').on('click', '.tab', function(e) {
+      $('.tab-content').hide();
+      var tab = $(this).attr('data-content');
+      console.log(tab);
+      $('#' + tab).fadeIn();
     });
-  });
-};
+    $('.main-nav .tab:first').click();
+  };
 
-projectView.renderIndexPage = function() {
-  Project.allProjects.forEach(function(a) {
-    $('#articles').append(a.toHtml('#project-template'));
-  });
-};
-// project.initNewArticlePage();
-// projectView.create();
-// projectView.render();
-projectView.handleMainNav();
-Project.fetchAll();
-projectView.setPreview();
+  projectView.setPreview = function() {
+    $('.article-body *:nth-of-type(n+2)').hide();
+    $('.view-more').on('click', function(event){
+      event.preventDefault();
+      if($(this).text() === 'View More') {
+        $(this).parent().find('*').fadeIn();
+        $(this).html('Show Less');
+      } else {
+        $('body').animate({
+          scrollTop: ($(this).parent().offset().top)
+        },200);
+        $(this).html('View More');
+        $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
+      }
+    });
+  };
+
+  // projectView.render = function() {
+  //   project.forEach(function(a) {
+  //     $('pre code').each(function(i, block) {
+  //       hljs.highlightBlock(block);
+  //     });
+  //   });
+  // };
+
+  projectView.renderIndexPage = function() {
+    Project.allProjects.forEach(function(a) {
+      $('#articles').append(a.toHtml('#project-template'));
+    });
+    projectView.handleMainNav();
+    projectView.setPreview();
+  };
+  // project.initNewArticlePage();
+  // projectView.create();
+  // projectView.render();
+  Project.fetchAll(projectView.renderIndexPage);
+
+  module.projectView = projectView;
+})(window);
